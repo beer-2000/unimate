f5417808d434abb83d093caed593d6bbab62cee4
jeje <jihyeseok@Jihyeui-MacBookPro.local>
2022-02-09 23:36:32 +0900

    update profile model, url

---
+++ unimate/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'unimate',  # DB name
+        'USER': 'root',  # DB account
+        'PASSWORD': '',  # DB account's password
+        'HOST': '127.0.0.1',  # DB address(IP)
+        'PORT': '3306',  # DB port(normally 3306)
+    }
+}
