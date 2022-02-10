682e7ddb17c7caacc0fccd4f67c446e6018ff5a0
Lee Woojin <dhkep03@gmail.com>
2022-02-10 20:32:07 +0900

    checking gitignore

--- unimate/my_settings.py
+++ unimate/my_settings.py
@@ -1,9 +1,10 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        #'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': 'unimate',  # DB name
         'USER': 'root',  # DB account
-        'PASSWORD': '',  # DB account's password
+        'PASSWORD': 'miami0306!',  # DB account's password
         'HOST': '127.0.0.1',  # DB address(IP)
         'PORT': '3306',  # DB port(normally 3306)
     }
